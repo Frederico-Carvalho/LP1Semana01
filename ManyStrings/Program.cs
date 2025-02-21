b95af5afae8c486a05a89a6fc53f85e23c9618fe
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 13:54:43 +0000

    Strings Exercicio 6

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
@@ -7,7 +8,17 @@ namespace ManyStrings
     public class Program
     {
         private static void Main(string[] args)
-        {
+        {   
+            Console.OutputEncoding = Encoding.UTF8;
+            double xx = 1.23456f;    
+            int ii = 19;
+
+            string f1 = $"{xx:f2}";
+            string f2 = $"{xx:p1}";
+
+            string g1 = $"{ii:x}";
+            string g2 = $"{ii:c}";
+
             string message = "Hello world!";
             message = "\b Alterar os conteudos da string";
             string Symbols = "\t \u03B2";
@@ -32,6 +43,10 @@ namespace ManyStrings
             Console.WriteLine(c);
             Console.WriteLine(d);
             Console.WriteLine(e);
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            Console.WriteLine(g1);
+            Console.WriteLine(g2);
         }
     }
 }
