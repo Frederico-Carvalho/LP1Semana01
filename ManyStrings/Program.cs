3201dc93148bd6eff8689f8088162373a085acb0
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 13:26:27 +0000

    Added Diferent Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string message = "Hello world!";
+            message = "\b Alterar os conteudos da string";
+            string Symbols = "\t \u03B2";
+            string seta = "\n \u003E";
+
+            Console.WriteLine(message);
+            Console.WriteLine(Symbols);
+            Console.WriteLine(seta);
+        }
+    }
+}
