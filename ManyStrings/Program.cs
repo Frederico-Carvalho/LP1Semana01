c6d7f09c42511fb6ddb0a9d79b3abc3f0e6122d0
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 13:41:52 +0000

    Strings Exercicio 5

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ManyStrings
 {
@@ -11,10 +12,26 @@ namespace ManyStrings
             message = "\b Alterar os conteudos da string";
             string Symbols = "\t \u03B2";
             string seta = "\n \u003E";
+            
+            int x = 5;
+            float y = 1.1f;
+
+            string a = "String " + "chama-se " + "String " + "a" + 2;
+            string b = "Outros tipos auto convertidos: " + "abc" + x;
+            string c = "Isto: " + x + "" + y;
+            c += " Soma Tudo"; 
+
+            string d = $"O valor de {x} e'  {x}";
+            string e = $"{x} mais {y} e' igual a {x + y}";
 
             Console.WriteLine(message);
             Console.WriteLine(Symbols);
             Console.WriteLine(seta);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
         }
     }
 }
