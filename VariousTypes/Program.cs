fc4b04cfd0f2a038dfe58ab62109383ec730565e
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 02:36:33 +0000

    Criação de Variáveis Inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            short a = 3;
+            int b = 15;
+            long c = 100000L;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
