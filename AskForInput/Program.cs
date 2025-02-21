929897a174edc2586472127798732451bff019da
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 14:39:06 +0000

    Exercicio 7

---
+++ AskForInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Insere um numero inteiro: ");
+            
+            string str = Console.ReadLine();
+            int i = int.Parse(str);
+            Console.WriteLine("Insere um numero real: ");
+
+            string str2 = Console.ReadLine();
+            float f = float .Parse(str2);
+
+
+            string x = $"{i} mais {f} e' igual a {i + f}";
+            
+            Console.WriteLine(x);
+        }
+    }
+}
