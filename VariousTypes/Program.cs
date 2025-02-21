dc356c0fe99fd671b01544e8ec659502f7dfd825
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 03:01:45 +0000

    Diferentes Variáveis de Char

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,9 +10,16 @@ namespace VariousTypes
             int b = 15;
             long c = 100000L;
 
+            char letra = 'A';
+            char matematica = '\u00B1';
+            char simbolo = '\u00A9';
+
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
+            Console.WriteLine(letra);
+            Console.WriteLine(matematica);
+            Console.WriteLine(simbolo);
         }
     }
 }
