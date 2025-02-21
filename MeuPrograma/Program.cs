7edea57a3256d369437e837611459925c64f25ac
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-02-21 01:12:01 +0000

    Summarys

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,12 +2,19 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Class Program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// 2 WriteLine command lines
+        /// </summary>
+        /// <param name="args">String</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bye World!");
+            Console.WriteLine("ByeWorld!");
         }
     }
 }
